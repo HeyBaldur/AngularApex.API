9f715a8db1f53d8d7450be171ed6e2541b5e3060
Rudy Jaubert <fillocj.rc@outlook.com>
2024-03-25 16:16:24 +0100

    Add project files.

---
+++ AngularApex.API/Extensions/AddDependencies.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+
+namespace AngularApex.API.Extensions
+{
+    public static partial class AddDependencies
+    {
+        public static IServiceCollection Dependencies(this IServiceCollection services)
+        {
+            return services;
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+# AngularApex.API
